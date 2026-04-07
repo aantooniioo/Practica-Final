3efabb5693b3a7c541dd247e703edad29f66ab72
Antonio Ángeles <aangeles@futurespace.es>
2026-04-07 15:29:05 +0200

    Acceso a asignación de empleados desde el panel principal

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <template>
-    <div>
-        <h1>Inicio</h1>
-        <p>Proyecto práctica final</p>
+    <div class="container mt-4">
+    <h2>Panel principal</h2>
+
+    <div class="d-flex gap-2 mt-3">
+
+        <button 
+        class="btn btn-primary"
+        @click="$router.push('/empleados')">
+        Empleados
+        </button>
+
+        <button 
+        class="btn btn-secondary"
+        @click="$router.push('/proyectos')">
+        Proyectos
+        </button>
+
+        <button 
+        class="btn btn-success"
+        @click="$router.push('/asignacion')">
+        Asignar empleados
+        </button>
+
     </div>
+</div>
 </template>
 
 <script>
