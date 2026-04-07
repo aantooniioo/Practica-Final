7f4dd493c13707f6750409dd06de748d56b6a893
Antonio Ángeles <aangeles@futurespace.es>
2026-04-07 15:56:57 +0200

    Añadido endpoint para consulta de asignaciones

--- backend/src/main/java/com/angeles/backend/controller/EmpleadoProyectoController.java
+++ backend/src/main/java/com/angeles/backend/controller/EmpleadoProyectoController.java
@@ -5,6 +5,8 @@ import com.angeles.backend.repository.EmpleadoProyectoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/asignaciones")
 @CrossOrigin(origins = "http://localhost:5173")
@@ -13,6 +15,13 @@ public class EmpleadoProyectoController {
     @Autowired
     private EmpleadoProyectoRepository repo;
 
+    // GET -> Ver todas las asignaciones
+    @GetMapping
+    public List<EmpleadoProyecto> listar() {
+        return repo.findAll();
+    }
+
+    // POST -> Asignar empleado a proyecto
     @PostMapping
     public EmpleadoProyecto asignar(@RequestBody EmpleadoProyecto ep){
         return repo.save(ep);
