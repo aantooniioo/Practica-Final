39ccf4af26655424bc97640392e7f339974ae6d5
Antonio Ángeles <aangeles@futurespace.es>
2026-04-07 15:18:41 +0200

    Implementado endpoint para asignar empleados a proyectos

--- backend/src/main/java/com/angeles/backend/controller/EmpleadoProyectoController.java
+++ backend/src/main/java/com/angeles/backend/controller/EmpleadoProyectoController.java
@@ -1,4 +1,20 @@
 package com.angeles.backend.controller;
 
+import com.angeles.backend.entity.EmpleadoProyecto;
+import com.angeles.backend.repository.EmpleadoProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/asignaciones")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmpleadoProyectoController {
+
+    @Autowired
+    private EmpleadoProyectoRepository repo;
+
+    @PostMapping
+    public EmpleadoProyecto asignar(@RequestBody EmpleadoProyecto ep){
+        return repo.save(ep);
+    }
 }
