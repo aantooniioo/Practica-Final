2ef89d28157eba2ad63d594a151389f5ed34212d
Antonio Ángeles <aangeles@futurespace.es>
2026-04-08 14:27:22 +0200

    Organización de servicios frontend para empleados, proyectos y asignaciones

--- frontend/src/services/empleadoProyectoService.js
+++ frontend/src/services/empleadoProyectoService.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/asignaciones";
 
-export function asignarEmpleado(data){
+export function asignarEmpleadoProyecto(data){
     return axios.post(API_URL, data);
+}
+
+export function getAsignaciones(id) {
+    return axios.get("http://localhost:8080/asignaciones");
 }
--- frontend/src/services/empleadoService.js
+++ frontend/src/services/empleadoService.js
@@ -2,14 +2,17 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/empleados";
 
+// Obtener empleados
 export function getEmpleados() {
     return axios.get(API_URL);
 }
 
+// Dar de baja
 export function bajaEmpleado(id){
     return axios.put(`${API_URL}/baja/${id}`);
 }
 
+// Crear empleado
 export function crearEmpleado(empleado){
     return axios.post(API_URL, empleado);
 }
