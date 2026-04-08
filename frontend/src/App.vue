a88012d478c533958befc62faccd20070c1eeb50
Antonio Ángeles <aangeles@futurespace.es>
2026-04-08 10:03:45 +0200

    Configurar Vuetify en el proyecto frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <router-view />
+  <v-app>
+
+    <!-- Barra superior de navegación -->
+    <v-app-bar color="black" dark>
+      <v-toolbar-title>Práctica Final - Gestión Future Space</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Navegación -->
+      <v-btn text @click="$router.push('/')">Home</v-btn>
+      <v-btn text @click="$router.push('/empleados')">Empleados</v-btn>
+      <v-btn text @click="$router.push('/proyectos')">Proyectos</v-btn>
+      <v-btn text @click="$router.push('/asignaciones')">Asignaciones</v-btn>
+    </v-app-bar>
+
+    <!-- Contenido principal -->
+    <v-main>
+      <router-view />
+    </v-main>
+
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
-};
+  name: "App"
+}
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+// Iconos Vuetify
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'dark'
+    }
+})
+
+// CSS Global
+import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
