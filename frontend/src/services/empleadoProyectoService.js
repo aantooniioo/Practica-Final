5c3fab2ec3640cc0d37d1c76343a0ef874dfdaaa
Antonio Ángeles <aangeles@futurespace.es>
2026-04-11 03:13:11 +0200

    Refactorización en services del frontend (empleados y proyectos)

--- frontend/src/services/empleadoProyectoService.js
+++ frontend/src/services/empleadoProyectoService.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/asignaciones";
 
+// Asignar empleado a proyecto
 export function asignarEmpleadoProyecto(data){
     return axios.post(API_URL, data);
 }
 
+// Obtener asignaciones de empleado por id
 export function getAsignaciones(id) {
     return axios.get("http://localhost:8080/asignaciones");
 }
--- frontend/src/services/proyectoService.js
+++ frontend/src/services/proyectoService.js
@@ -2,22 +2,27 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/proyectos";
 
+// Obtener proyectos
 export function getProyectos() {
     return axios.get(API_URL);
 }
 
+// Crear proyecto
 export function crearProyecto(proyecto) {
     return axios.post(API_URL, proyecto);
 }
 
+// Dar de baja proyecto
 export function bajaProyecto(id) {
     return axios.put(`${API_URL}/baja/${id}`);
 }
 
+// Obtener proyecto
 export function getProyectoById(id){
     return axios.get(`${API_URL}/${id}`);
 }
 
+// Editar proyecto
 export function editarProyecto(id, data){
     return axios.put(`${API_URL}/${id}`, data);
 }
