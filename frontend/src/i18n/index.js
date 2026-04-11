75abc876175b5247886f1145ad56688aedbd2f86
Antonio Ángeles <aangeles@futurespace.es>
2026-04-11 03:11:57 +0200

    Mejoras en traducciones i18n y nuevas claves

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,8 +1,10 @@
 import { createI18n } from "vue-i18n";
 
+// Importa las traducciones
 import es from './es.json';
 import en from './en.json';
 
+// Configura el i18n
 const i18n = ({
   legacy: false,
   locale: localStorage.getItem('lang') || 'es',
@@ -12,5 +14,7 @@ const i18n = ({
     en
   }
 })
+
+// Exporta el i18n para poder usarlo en la app
 
 export default createI18n(i18n);
